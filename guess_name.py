ed38943146d72a70003e4d75b3c18b8c5e1004a1
ulhaqjackyhaw <ulhaqjackyhaw@gmail.com>
2023-12-04 13:53:06 +0700

    commit pertama

---
+++ guess_name.py
@@ -0,0 +1,15 @@
+def guess_number():
+    angka_rahasia=9
+    batas=3
+    penghitung=0
+
+    while penghitung < batas:
+        user = int(input("Masukan angka > "))
+        if user == angka_rahasia:
+            print("Selamat tebakan anda benar")
+            break
+        else :
+            print("tebakan anda salah")
+            penghitung += 1
+    else :
+        print("gagal")
